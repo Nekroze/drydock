eb386a01d5a2d8dba8e0a4fd75b7402edefd3e9e
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-02-20 12:15:29 +1000

    changed exposed_ports to dict for alter mapping and added commands

--- drydock/duster.py
+++ drydock/duster.py
@@ -1,22 +1,24 @@
-"""DryDock cluster specification"""
+"""DryDock cluster specification."""
 
 
 class Container(object):
     """A docker container specification."""
     def __init__(self, name, base="ubuntu", exposed_ports=None,
-                 external=False, http_port=0, https_port=0):
+                 external=False, http_port=0, https_port=0, commands=None):
         self.name = name
         self.base = base
-        self.exposed_ports = [] if not exposed_ports else exposed_ports
+        self.exposed_ports = {}
+        self.exposed_ports.update(exposed_ports)
         self.http_port = 0
         self.https_port = 0
         self.external = external
+        self.commands = [] if not commands else commands
 
 
 class MetaContainer(Container):
     """A container that stores containers."""
-    def __init__(self, name, domain, subcontainers, base="nekroze/drydock"):
-        super(MetaContainer, self).__init__(name=name, base=base)
+    def __init__(self, name, domain, subcontainers, commands=None, base="nekroze/drydock"):
+        super(MetaContainer, self).__init__(name=name, base=base, commands=commands)
         self.domain = domain
         self.containers = {}
         self.reverse_proxies = {}
@@ -26,7 +28,7 @@ class MetaContainer(Container):
 
     def add_container(self, container):
         self.containers[container.name] = container
-        self.exposed_ports.extend(container.exposed_ports)
+        self.exposed_ports.update(container.exposed_ports)
 
         fqdn = container.name + '.' + self.domain
         self.reverse_proxies[fqdn] = {
