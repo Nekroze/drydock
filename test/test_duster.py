3d8375d30a09a4efdd07b74da6ef30b5834773ea
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-02-21 11:07:28 +1000

    Implemented first basic test module for duster.Container construction

---
+++ test/test_duster.py
@@ -0,0 +1,32 @@
+"""
+Tests for `drydock.duster` module.
+"""
+import pytest
+from drydock.duster import Container, MetaContainer
+import yaml
+
+
+class TestCotnainer(object):
+
+    def test_construction(self):
+        config = """---
+name: blog
+base: nekroze/wordpress
+domain: nekroze.com
+exposed_ports:
+    22: 22
+    2222: 222
+http_port: 8081
+https_port: 4431
+external: No
+..."""
+        container = Container(**yaml.load(config))
+        assert container.name == "blog"
+        assert container.base == "nekroze/wordpress"
+        assert container.domain == "nekroze.com"
+        assert container.exposed_ports == {22:22, 2222:222}
+        assert container.http_port == 8081
+        assert container.https_port == 4431
+        assert container.external is False
+        assert container.skyfqdn == "blog.wordpress.containers.drydock"
+        assert container.fqdn == "blog.nekroze.com"
