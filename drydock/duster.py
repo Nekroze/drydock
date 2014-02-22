8852083eddfe64baf21744bf590e49799adfeff4
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-02-22 12:32:58 +1000

    added meta container argument passing to parent

--- drydock/duster.py
+++ drydock/duster.py
@@ -51,8 +51,8 @@ autorestart=true""".format(self.name)
 
 class MetaContainer(Container):
     """A container that stores containers."""
-    def __init__(self, name, domain, subcontainers, base="nekroze/drydock"):
-        super(MetaContainer, self).__init__(name=name, base=base, domain=domain)
+    def __init__(self, name, domain, subcontainers, base="nekroze/drydock", *args, **kwargs):
+        super(MetaContainer, self).__init__(*args, name=name, base=base, domain=domain, **kwargs)
         self.containers = {}
         self.reverse_proxies = {}
 
