0829dc5b7e8197767132f574d97f41cef5dec715
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-02-24 12:05:12 +1000

    Overhauled duster module to use commands to build containers inplace rather then construct a Dockerfile

--- drydock/duster.py
+++ drydock/duster.py
@@ -18,7 +18,6 @@ class Container(object):
         self.https_port = https_port
         self.external = external
         self.volumes = volumes if volumes else []
-        self.commands = []
         self.skyfqdn = '.'.join([self.name, self.base.split('/')[-1],
                                  "containers", "drydock"])
 
@@ -30,51 +29,38 @@ class Container(object):
         else:
             self.fqdn = self.name + '.' + self.domain
 
-    def get_container_commands(self):
-        """
-        Return a list of commands required to construct and use this
-        container specification.
-        """
-        commands = []
-        run = 'docker run -d'.split(' ')
-
-        run.append("--name {0}".format(self.name))
-
+    def get_portmaps(self):
+        """Get docker command port maps."""
+        output = []
         for external in sorted(self.exposed_ports.keys()):
-            run.append("-p {0}:{1}".format(external,
+            output.append("-p {0}:{1}".format(external,
                                            self.exposed_ports[external]))
+        return output
 
+    def get_volumemaps(self):
+        """Return docker command volume maps."""
+        output = []
         for path in self.volumes:
-            run.append("-v /var/lib/{0}{1}:{1}".format(self.name, path))
+            output.append("-v /var/lib/{0}{1}:{1}".format(self.name, path))
+        return output
 
-        run.append(self.base)
-        commands.append("RUN " + ' '.join(run))
-        commands.append(
-            "ADD supervisor/{0}.conf /etc/supervisor/conf.d/{0}.conf".format(
-                self.name))
+    def get_docker_command(self):
+        """Return the docker command required to create this container."""
+        cmd = 'docker run -d'.split(' ')
+        cmd.append('-name ' + self.name)
 
-        if self.http_port or self.https_port:
-            commands.append(
-                "ADD sites/{0} /etc/nginx/sites-enabled/{0}".format(self.fqdn))
+        cmd.extend(self.get_portmaps())
+        cmd.extend(self.get_volumemaps())
 
-        commands.extend(self.commands)
-        return '\n'.join(commands)
+        cmd.append(self.base)
+        return cmd
 
     def get_supervisor_config(self):
-        """
-        Write this containers supervisor configuration file to
-        ``./supervisor/{Container.name}.conf``.
-        """
-        return """[program:{0}]
-command=docker start {0}
-autostart=true
-autorestart=true""".format(self.name)
+        """Get a rendered supervisor configuration file for thiscontainer."""
+        return templates.SUPERVISOR_CONTAINER.format(self.name)
 
     def get_nginx_config(self):
-        """
-        Write this containers nginx site configuration to
-        ``./sites/{Container.fqdn}``
-        """
+        """Get a rendered nginx configuration file for this container."""
         return templates.render_nginx_config(self)
 
 
@@ -92,7 +78,7 @@ class MetaContainer(Container):
             self.add_container(Container(**sub))
 
     def set_domain(self, domain):
-        """Set fqdn and domain."""
+        """Set fqdn and domain to the same value."""
         self.domain = domain
         self.fqdn = domain
 
@@ -112,67 +98,18 @@ class MetaContainer(Container):
             "https": container.https_port
         }
 
-    def get_portmaps(self):
-        """return docker args for subcontainer port maps."""
-        template = "-p {0}:{1}"
-        portmaps = [template.format("80", "80"), template.format("443", "443")]
-
+    def get_nginx_config(self):
+        """Get a rendered nginx configuration file for all containers."""
+        output = []
         for name in sorted(self.containers.keys()):
             container = self.containers[name]
-            for port in sorted(container.exposed_ports.keys()):
+            output.append(container.get_nginx_config())
+        return '\n'.join(output)
 
-                portmap = template.format(port, port)
-                if portmap not in portmaps:
-                    portmaps.append(portmap)
-
-        return ' '.join(portmaps)
-
-    def get_volumemaps(self):
-        """return docker args for subcontainer volume maps."""
-        volumes = []
-
-        for volume in self.volumes:
-            volumes.append("-v {statedir}{volume}:{volume}".format(
-                statedir="/var/lib/" + self.name, volume=volume))
-
+    def get_supervisor_config(self):
+        """Get a rendered supervisor configuration file for all containers."""
+        output = []
         for name in sorted(self.containers.keys()):
             container = self.containers[name]
-
-            for volume in container.volumes:
-                volumes.append("-v {statedir}/{name}{volume}:{volume}".format(
-                    statedir="/var/lib/" + self.name, name=container.name,
-                    volume=volume))
-
-        return ' ' + ' '.join(set(volumes))
-
-    def get_docker_commands(self):
-        """Get commands required to build and run the final product."""
-        commands = ["docker build -t {name}-img .".format(name=self.name),
-                    "docker run -privileged -d -t --name {0} {0}-img {1}{2}".format(
-                        self.name, self.get_portmaps(), self.get_volumemaps())]
-        return "\n".join(commands)
-
-    def get_dockerfile(self):
-        """Render the dockerfile according to specifications."""
-        commands = ["FROM " + self.base, ""]
-        ports = [80, 443]
-        volumes = []
-
-        for name in sorted(self.containers.keys()):
-            container = self.containers[name]
-
-            ports.extend(sorted(list(container.exposed_ports.keys())))
-            commands.append(container.get_container_commands())
-            commands.append("")
-
-            for volume in container.volumes:
-                volumes.append("/var/lib/{0}{1}".format(container.name,
-                                                        volume))
-
-        commands.append("EXPOSE " + ' '.join([str(port) for port in ports]))
-
-        volumes = ['"{0}"'.format(volume) for volume in volumes]
-        if volumes:
-            commands.append("VOLUME [{0}]".format(", ".join(set(volumes))))
-
-        return '\n'.join(commands)
+            output.append(container.get_supervisor_config())
+        return '\n'.join(output)
