c30e6ebade41c4cb447a3623fc274e72b780a2ea
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2014-02-25 01:36:21 +1000

    fix setup.py version number

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='drydock',
-    version='0.1.0',
+    version='0.1.1',
     description='A Docker cluster construction utility.',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Taylor "Nekroze" Lawson',
